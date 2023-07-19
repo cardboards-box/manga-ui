69f34d2a28220758a68b3d09868959e374ddb9be
calico-crusade <holycardboardquest@gmail.com>
2023-07-19 14:13:50 -0400

    Making external manga route to the viewer

--- src/components/volume-card/VolumeCard.vue
+++ src/components/volume-card/VolumeCard.vue
@@ -1,27 +1,26 @@
 <template>
 <div class="volume-card" :class="{ 'version': version, 'no-buttons': !hasButtons }">
-    <NuxtLink :to="url" :class="{ 'active': isRead }" class="cell">
+    <NuxtLink :to="url" :class="{ 'active': isRead }" class="cell" :target="external ? '_black': ''" :title="external ? 'External Manga' : 'Read ' + title">
+        <Icon v-if="external">ungroup</Icon>
         <Icon v-if="isRead">done_all</Icon>
         <Icon v-if="chapter.id === progress?.mangaChapterId">
             auto_stories
         </Icon>
-        {{ chapter.volume ? 'Vol. ' + chapter.volume + ' ' : '' }}
-        Ch. {{ chapter.ordinal }}
-        {{ chapter.title ? ' - ' + chapter.title : '' }}
+        {{ title }}
     </NuxtLink>
     <span class="cell">
         <Icon>schedule</Icon>&nbsp;
         <Date :date="chapter.createdAt.toString()" format="partial" />
     </span>
     <div class="cell btns" v-if="hasButtons">
-        <IconBtn 
+        <IconBtn
             other-classes="cell"
             v-if="currentUser"
             :loading="loading"
             :icon="isRead ? 'visibility_off' : 'visibility'"
             @click="toggleRead"
         />
-        <IconBtn 
+        <IconBtn
             v-if="hasVersions"
             other-classes="cell"
             :loading="loading"
@@ -31,7 +30,7 @@
     </div>
 </div>
 </template>
-    
+
 <script setup lang="ts">
 import { Chapter, Progress } from '~/models';
 
@@ -57,13 +56,25 @@ const props = defineProps<{
 const loading = ref(false);
 const isRead = computed(() => props.modelValue);
 const isOpen = computed(() => props.open ?? false);
+const external = computed(() => props.chapter.externalUrl);
 const url = computed(() => {
+    if (external.value) return external.value;
+
     let base = `/manga/${props.chapter.mangaId}/${props.chapter.id}`;
     if (props.chapter.id === props.progress?.mangaChapterId)
         base += `?page=${(props.progress.pageIndex ?? 0) + 1}`;
     return base;
 });
 const hasButtons = computed(() => !props.version && (!!currentUser.value || props.hasVersions));
+const title = computed(() => {
+    let output = '';
+    if (props.chapter.volume)
+        output += `Vol. ${props.chapter.volume} `;
+    output += `Ch. ${props.chapter.ordinal}`;
+    if (props.chapter.title)
+        output += ` - ${props.chapter.title}`;
+    return output;
+});
 
 const toggleRead = async () => {
     if (!currentUser.value) return;
@@ -75,7 +86,7 @@ const toggleRead = async () => {
         emits('update:modelValue', !props.modelValue);
 }
 </script>
-    
+
 <style lang="scss" scoped>
 .volume-card {
     display: grid;
